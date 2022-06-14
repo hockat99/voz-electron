bbcff14e4f38af645d9cfa7b182f98108849f895
hockat99 <107326852+hockat99@users.noreply.github.com>
2022-06-14 18:47:08 -0500

    Add files via upload

---
+++ voz.js
@@ -0,0 +1,37 @@
+if (annyang) {
+
+    let voices;
+
+    let utter = new SpeechSynthesisUtterance();
+
+    utter.rate = 1.5;
+    utter.pitch = 1.2;
+    utter.lang = 'es-MX';
+    
+    window.speechSynthesis.onvoiceschanged = function () {
+        voices = window.speechSynthesis.getVoices();
+        console.log(voices);
+    };
+
+
+    var commands = {
+        'hola': function() {
+          console.log('funciono!!!!!!!!!!!!!!!!!!!!');
+          console.log('funciono!!!!!!!!!!!!!!!!!!!!');
+          console.log('funciono!!!!!!!!!!!!!!!!!!!!');
+        }
+      };
+    
+    function voice(Text) {
+      utter.text = Text;
+      utter.voice = voices[4];
+      window.speechSynthesis.speak(utter);
+    }
+
+
+      
+    annyang.addCommands(commands);
+    annyang.setLanguage('es-PE');
+    annyang.start({autorestar: false, continuos: true});
+
+};
